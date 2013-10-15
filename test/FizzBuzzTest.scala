720ad1e1ddd25e34b1f234e5e708a7ddcf45162b
emccallum <mccallum.ek@gmail.com>
2013-10-15 10:21:09 -0700

    Adding test and class files

---
+++ test/FizzBuzzTest.scala
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Thoughtworks
+ * Date: 10/15/13
+ * Time: 10:20 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class FizzBuzzTest {
+
+}
